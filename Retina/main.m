eb084220a724a2158298c6407913cb64be7fd9ff
Brian William Wolter <brian@woltergroup.net>
2012-09-18 11:13:17 -0400

    Initial Commit

---
+++ Retina/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Retina
+//
+//  Created by Brian William Wolter on 9/18/12.
+//  Copyright (c) 2012 Wolter Group New York. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
